5208b1d8c04f2425816cb90ef9dad9eae27d1d5f
andrefurlan <andrefurlan@gmail.com>
2015-12-01 16:57:53 -0800

    few tests for gameOver

--- Tests.hs
+++ Tests.hs
@@ -15,15 +15,50 @@ test1 = TestCase (
         "WWW-WW-------BB-BBB") -- expected output
 
 
-test_gameOver = TestCase (
+test_samemove1 = TestCase (
     assertEqual
         "Board present in list"
         (gameOver
             [W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]
             [[W,B,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B],[W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B],[W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]]
             3)
+        True)
+
+test_samemove2 = TestCase (
+    assertEqual
+        "Board not present in list"
+        (gameOver
+            [W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]
+            [[W,B,W,D,W,W,D,D,D,D,D,D,D,D,B,D,B,B,B],[W,D,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B],[D,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]]
+            3)
+        True)
+
+test_fewW1 = TestCase (
+    assertEqual
+        "Too few W"
+        (gameOver
+            [D,W,D,D,D,W,D,D,D,D,D,D,D,B,B,D,B,B,B]
+            [[W,B,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B],[W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B],[W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]]
+            3)
+        True)
+
+test_fewW2 = TestCase (
+    assertEqual
+        "Equal W"
+        (gameOver
+            [W,D,D,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]
+            [[W,B,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B],[W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B],[W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]]
+            3)
         False)
 
+test_fewW3 = TestCase (
+    assertEqual
+        "Too many W"
+        (gameOver
+            [W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]
+            [[W,B,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B],[W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B],[W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]]
+            3)
+        False)
 
-gameOver_tests = TestList [test_gameOver]
+gameOver_tests = TestList [test_samemove1, test_samemove2, test_fewW1, test_fewW2, test_fewW3]
 tests = TestList [TestLabel "gameOver" gameOver_tests]
