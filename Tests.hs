d9efd208f288ea85739a60c1c764005240e3f04b
andrefurlan <andrefurlan@gmail.com>
2015-12-01 16:48:06 -0800

    better way to write tests

--- Tests.hs
+++ Tests.hs
@@ -1,59 +1,29 @@
-{-# OPTIONS_GHC -F -pgmF htfpp #-}
-
-import Test.Framework
+import Test.HUnit
 
 import Crusher
 
-test_t = do assertEqual "WWW-WW-------BB-BBB" (boardToStr [W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B])
-            assertEqual "WWW-WW-------BB-BBB" (boardToStr [W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B])
-
-test_t2 = do assertEqual "WWW-WW-------BB-BBB" (boardToStr [W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B])
-             assertEqual "WWW-WW-------BB-BB" (boardToStr [W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B])
-
-{- To run this test using the Haskell Unit Test framework,
-   first compile the code using ghc -o tests Tests.hs
-   and then execute it using tests.exe or .\tests
-
+-- example
+{-  To run this test using the Haskell Unit Test framework,
+    first compile the code using ghci and use the command
+    runTestTT tests
 -}
-runTests :: Bool
-runTests =
-    testsTrToBoard &&
-    testboardToStr &&
-    testcrusher &&
-    testgenerateGrid &&
-    testgameOver
 
-testboardToStr :: Bool
-testboardToStr =
-    -- test cases
-    boardToStr [W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]
-        == "WWW-WW-------BB-BBB" &&
+test1 = TestCase (
+    assertEqual
+        "aaa" -- annotation
+        (boardToStr [W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]) -- function call
+        "WWW-WW-------BB-BBB") -- expected output
 
-    boardToStr [W,W,W,D,W,W,D,D,D,B,D,D,D,B,B,D,B,B,B]
-        == "WWW-WW---B---BB-BBB"
 
-testsTrToBoard :: Bool
-testsTrToBoard =
-    -- test cases
-    sTrToBoard "WWW-WW-------BB-BBB"
-        == [W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]
+test_gameOver = TestCase (
+    assertEqual
+        "Board present in list"
+        (gameOver
+            [W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]
+            [[W,B,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B],[W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B],[W,W,W,D,W,W,D,D,D,D,D,D,D,B,B,D,B,B,B]]
+            3)
+        False)
 
-testcrusher :: Bool
-testcrusher =
-    crusher ["W------------BB-BBB","----W--------BB-BBB","-W-----------BB-BBB"] 'W' 2 3
-        == ["W------------BB-BBB","----W--------BB-BBB","-W-----------BB-BBB"]
 
-testgenerateGrid :: Bool
-testgenerateGrid =
-    generateGrid 3 2 4 []
-        == [       (0,0),(1,0),(2,0),
-                (0,1),(1,1),(2,1),(3,1),
-             (0,2),(1,2),(2,2),(3,2),(4,2),
-                (0,3),(1,3),(2,3),(3,3),
-                   (0,4),(1,4),(2,4)]
-
-testgameOver :: Bool
-testgameOver =
-    True == True
-
-main = htfMain htf_thisModulesTests
+gameOver_tests = TestList [test_gameOver]
+tests = TestList [TestLabel "gameOver" gameOver_tests]
